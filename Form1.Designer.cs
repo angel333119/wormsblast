c5b36820f2d44a7ee3d4a0b7fda1c43d9f32b936
Caio <caio333119@gmail.com>
2023-08-25 19:15:00 -0300

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -71,7 +71,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Worms Blast PC/PS2 Audio extractor/inserter By Angel333119";
+            this.Text = "Worms Blast PC/PS2 Audio/Text extractor/inserter By Angel333119";
             this.ResumeLayout(false);
 
         }
