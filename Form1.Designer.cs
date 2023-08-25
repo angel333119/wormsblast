10d10552781592c88e0ee7bf0ee3a4c00c6786a3
Caio <caio333119@gmail.com>
2023-08-24 21:55:08 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace worm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 254);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Extrair";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(367, 254);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Inserir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::worm.Properties.Resources.back;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(538, 313);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Worms Blast PC/PS2 Audio extractor/inserter By Angel333119";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace worm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Arquivo AUD|*.aud|Todos os arquivos (*.*)|*.*";
+            openFileDialog1.Title = "Escolha um arquivo .AUD...";
+            openFileDialog1.Multiselect = true;
+
+            int magic = 0x46464952;
+            long WAVEfmt = 0x20746D6645564157;
+            long parte1 = 0x0001000100000010;
+            long parte2 = 0x0000AC4400005622;
+            long parte3 = 0x6174616400100002;
+            int tamanho;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                foreach (String file in openFileDialog1.FileNames)
+                {
+                    using (FileStream stream = File.Open(file, FileMode.Open))
+                    {
+                        BinaryReader br = new BinaryReader(stream);
+
+                        br.BaseStream.Seek(0x48, SeekOrigin.Begin);
+
+                        int quantidadedewav = br.ReadInt32();
+
+                        br.BaseStream.Seek(0x114, SeekOrigin.Begin);
+
+                        int pulo = br.ReadInt32();
+
+                        br.BaseStream.Seek(0x118, SeekOrigin.Begin);
+
+                        int pulo2 = br.ReadInt32();
+
+                        br.BaseStream.Seek(pulo * 4 + pulo2 + 4, SeekOrigin.Current);
+
+                        int verificador = br.ReadInt32();
+
+                        int tamanhoarquivoatual = 0;
+
+                        br.BaseStream.Seek(-8, SeekOrigin.Current);
+
+                        if (verificador == 0x70474156) // Arquivo de PS2 - VAG
+                        {
+                            for (int i = 1; i <= quantidadedewav + 1; i++)
+                            {
+                                if (i <= quantidadedewav)
+                                {
+                                    tamanhoarquivoatual = br.ReadInt32();
+
+                                    tamanho = tamanhoarquivoatual;
+
+                                    byte[] arquivo = br.ReadBytes(tamanhoarquivoatual);
+
+                                    string caminhodapasta = Path.Combine(Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file));
+                                    Directory.CreateDirectory(caminhodapasta);
+
+                                    string arquivowav = Path.Combine(caminhodapasta, i + ".vag");
+
+                                    using (BinaryWriter bw = new BinaryWriter(File.OpenWrite(arquivowav)))
+                                    {
+                                        bw.Write(arquivo);
+                                    }
+                                }
+                                else
+                                {
+                                    tamanhoarquivoatual = br.ReadInt32();
+
+                                    br.BaseStream.Seek(-4, SeekOrigin.Current);
+
+                                    tamanho = tamanhoarquivoatual;
+
+                                    byte[] arquivo = br.ReadBytes(tamanhoarquivoatual);
+
+                                    string caminhodapasta = Path.Combine(Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file));
+                                    Directory.CreateDirectory(caminhodapasta);
+
+                                    string arquivowav = Path.Combine(caminhodapasta, i + ".bin");
+
+                                    using (BinaryWriter bw = new BinaryWriter(File.OpenWrite(arquivowav)))
+                                    {
+                                        bw.Write(arquivo);
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            for (int i = 1; i <= quantidadedewav + 1; i++)
+                            {
+                                if (i <= quantidadedewav)
+                                {
+                                    tamanhoarquivoatual = br.ReadInt32();
+
+                                    tamanho = tamanhoarquivoatual;
+
+                                    byte[] arquivo = br.ReadBytes(tamanhoarquivoatual);
+
+                                    string caminhodapasta = Path.Combine(Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file));
+                                    Directory.CreateDirectory(caminhodapasta);
+
+                                    string arquivowav = Path.Combine(caminhodapasta, i + ".wav");
+
+                                    using (BinaryWriter bw = new BinaryWriter(File.OpenWrite(arquivowav)))
+                                    {
+                                        bw.Write(magic);
+                                        bw.Write(tamanho + 0x24);
+                                        bw.Write(WAVEfmt);
+                                        bw.Write(parte1);
+                                        bw.Write(parte2);
+                                        bw.Write(parte3);
+                                        bw.Write(tamanho);
+                                        bw.Write(arquivo);
+                                    }
+                                }
+                                else
+                                {
+                                    tamanhoarquivoatual = br.ReadInt32();
+
+                                    br.BaseStream.Seek(-4, SeekOrigin.Current);
+
+                                    tamanho = tamanhoarquivoatual;
+
+                                    byte[] arquivo = br.ReadBytes(tamanhoarquivoatual);
+
+                                    string caminhodapasta = Path.Combine(Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file));
+                                    Directory.CreateDirectory(caminhodapasta);
+
+                                    string arquivowav = Path.Combine(caminhodapasta, i + ".bin");
+
+                                    using (BinaryWriter bw = new BinaryWriter(File.OpenWrite(arquivowav)))
+                                    {
+                                        bw.Write(arquivo);
+                                    }
+                                }
+                            }
+                        }                        
+                        MessageBox.Show("Terminado!", "AVISO!");
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Arquivo AUD|*.aud|Todos os arquivos (*.*)|*.*";
+            openFileDialog1.Title = "Escolha um arquivo .AUD...";
+            openFileDialog1.Multiselect = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                foreach (String file in openFileDialog1.FileNames)
+                {
+                    using (FileStream stream = File.Open(file, FileMode.Open))
+                    {
+                        string caminhodapasta = Path.Combine(Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file));
+
+                        if (Directory.Exists(caminhodapasta))
+                        {
+                            BinaryReader br = new BinaryReader(stream);
+                            BinaryWriter bw = new BinaryWriter(stream);
+
+                            br.BaseStream.Seek(0x48, SeekOrigin.Begin);
+
+                            int quantidadedewav = br.ReadInt32();
+
+                            br.BaseStream.Seek(0x114, SeekOrigin.Begin);
+
+                            int pulo = br.ReadInt32();
+
+                            br.BaseStream.Seek(0x118, SeekOrigin.Begin);
+
+                            int pulo2 = br.ReadInt32();
+
+                            br.BaseStream.Seek(pulo * 4 + pulo2 + 4, SeekOrigin.Current);
+
+                            int verificador = br.ReadInt32();
+
+                            int tamanhoarquivoatual = 0;
+
+                            stream.SetLength(0x800);
+
+                            br.BaseStream.Seek(-8, SeekOrigin.Current);
+
+                            if (verificador == 0x70474156) // Arquivo de PS2 - VAG
+                            {
+                                for (int i = 1; i <= quantidadedewav + 1; i++)
+                                {
+                                    if (i <= quantidadedewav)
+                                    {
+                                        byte[] bytesarquivoaudio = File.ReadAllBytes(Path.Combine(caminhodapasta, i + ".vag"));
+
+                                        int tamanhonovo = bytesarquivoaudio.Length;
+
+                                        bw.Write(tamanhonovo);
+                                        bw.Write(bytesarquivoaudio);
+
+                                    }
+                                    else
+                                    {
+                                        byte[] bytesarquivoaudio = File.ReadAllBytes(Path.Combine(caminhodapasta, i + ".bin"));
+
+                                        bw.Write(bytesarquivoaudio);
+                                    }
+                                }
+                            }
+                            else // Arquivo PC
+                            {
+                                for (int i = 1; i <= quantidadedewav + 1; i++)
+                                {
+                                    if (i <= quantidadedewav)
+                                    {
+                                        byte[] bytesarquivoaudio = File.ReadAllBytes(Path.Combine(caminhodapasta, i + ".wav"));
+
+                                        int novoTamanho = bytesarquivoaudio.Length - 0x2C;
+                                        byte[] novoArray = new byte[novoTamanho];
+                                        Array.Copy(bytesarquivoaudio, 0x2C, novoArray, 0, novoTamanho);
+
+                                        bw.Write(novoTamanho);
+                                        bw.Write(novoArray);
+                                    }
+                                    else
+                                    {
+                                        byte[] bytesarquivo = File.ReadAllBytes(Path.Combine(caminhodapasta, i + ".bin"));
+
+                                        bw.Write(bytesarquivo);
+                                    }
+                                }
+                            }
+                            MessageBox.Show("Terminado!", "AVISO!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("A pasta \"" + Path.GetFileNameWithoutExtension(file) + "\" não foi encontrada", "AVISO!");
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
